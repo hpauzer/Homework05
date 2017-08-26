32990b85352c8ec864c25bf9644ed7dfe9bc5cc7
Heather Pauzer <heather.pauzer@gmail.com>
2017-08-25 22:16:37 -0500

    upload homework

---
+++ assets/javascript/app.js
@@ -0,0 +1,202 @@
+(function () {
+    const myQuestions = [
+        {
+            question: "Who is not an Avenger?",
+            answers: {
+                a: "Thor",
+                b: "Black Panther",
+                c: "Drax"
+            },
+            correctAnswer: "c"
+    },
+        {
+            question: "What is Spiderman's real name?",
+            answers: {
+                a: "Bruce Wayne",
+                b: "Tony Stark",
+                c: "Peter Parker"
+            },
+            correctAnswer: "c"
+    },
+        {
+            question: "Who is Barry Allen?",
+            answers: {
+                a: "Ant Man",
+                b: "The Flash",
+                c: "Hawkeye",
+
+            },
+            correctAnswer: "b"
+    }
+  ];
+
+    function buildQuiz() {
+        // we'll need a place to store the HTML output
+        const output = [];
+
+        // for each question...
+        myQuestions.forEach((currentQuestion, questionNumber) => {
+            // we'll want to store the list of answer choices
+            const answers = [];
+
+            // and for each available answer...
+            for (letter in currentQuestion.answers) {
+                // ...add an HTML radio button
+                answers.push(
+                    `<label>
+             <input type="radio" name="question${questionNumber}" value="${letter}">
+              ${letter} :
+              ${currentQuestion.answers[letter]}
+           </label>`
+                );
+            }
+
+            // add this question and its answers to the output
+            output.push(
+                `<div class="slide">
+           <div class="question"> ${currentQuestion.question} </div>
+           <div class="answers"> ${answers.join("")} </div>
+         </div>`
+            );
+        });
+
+        // finally combine our output list into one string of HTML and put it on the page
+        quizContainer.innerHTML = output.join("");
+    }
+
+    function showResults() {
+        // gather answer containers from our quiz
+        const answerContainers = quizContainer.querySelectorAll(".answers");
+
+        // keep track of user's answers
+        let numCorrect = 0;
+
+        // for each question...
+        myQuestions.forEach((currentQuestion, questionNumber) => {
+            // find selected answer
+            const answerContainer = answerContainers[questionNumber];
+            const selector = `input[name=question${questionNumber}]:checked`;
+            const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+            // if answer is correct
+            if (userAnswer === currentQuestion.correctAnswer) {
+                // add to the number of correct answers
+                numCorrect++;
+
+                // color the answers green
+                answerContainers[questionNumber].style.color = "lightgreen";
+            } else {
+                // if answer is wrong or blank
+                // color the answers red
+                answerContainers[questionNumber].style.color = "red";
+            }
+        });
+
+        // show number of correct answers out of total
+        resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+    }
+
+    function showSlide(n) {
+        slides[currentSlide].classList.remove("active-slide");
+        slides[n].classList.add("active-slide");
+        currentSlide = n;
+
+        if (currentSlide === 0) {
+            previousButton.style.display = "none";
+        } else {
+            previousButton.style.display = "inline-block";
+        }
+
+        if (currentSlide === slides.length - 1) {
+            nextButton.style.display = "none";
+            submitButton.style.display = "inline-block";
+        } else {
+            nextButton.style.display = "inline-block";
+            submitButton.style.display = "none";
+        }
+    }
+
+    function showNextSlide() {
+        showSlide(currentSlide + 1);
+    }
+
+    function showPreviousSlide() {
+        showSlide(currentSlide - 1);
+    }
+
+    const quizContainer = document.getElementById("quiz");
+    const resultsContainer = document.getElementById("results");
+    const submitButton = document.getElementById("submit");
+
+    // display quiz right away
+    buildQuiz();
+
+    const previousButton = document.getElementById("previous");
+    const nextButton = document.getElementById("next");
+    const slides = document.querySelectorAll(".slide");
+    let currentSlide = 0;
+
+    showSlide(0);
+
+    // on submit, show results
+    submitButton.addEventListener("click", showResults);
+    previousButton.addEventListener("click", showPreviousSlide);
+    nextButton.addEventListener("click", showNextSlide);
+})();
+
+//Timer Functions
+
+var myCounter = new Countdown({
+    seconds: 15, // number of seconds to count down
+    onUpdateStatus: function (sec) {
+        console.log(sec);
+        $(".sec").html("Time Remaining: " + sec);
+    }, // callback for each second
+    onCounterEnd: function () {
+        console.log("Game Over");
+        $(".seconds").html("Time is up!" + response.seconds);
+        myCounter.stop();
+    } // final action
+});
+
+
+myCounter.start();
+
+function Countdown(options) {
+    var timer,
+        instance,
+        seconds = options.seconds || 10,
+        updateStatus = options.onUpdateStatus || function () {},
+        counterEnd = options.onCounterEnd || function () {};
+
+    function decrementCounter() {
+        updateStatus(seconds);
+        if (seconds === 0) {
+            counterEnd();
+            this.stop();
+        }
+        seconds--;
+    }
+
+    this.start = function () {
+        clearInterval(timer);
+        timer = 0;
+        seconds = options.seconds;
+        timer = setInterval(decrementCounter, 1000);
+    };
+
+    this.stop = function () {
+        clearInterval(timer);
+    };
+
+    function countdown() {
+        var i = document.getElementById('counter');
+        i.innerHTML = parseInt(i.innerHTML) - 1;
+        if (parseInt(i.innerHTML) == 0) {
+            clearInterval(timerId);
+        }
+    }
+    var timerId = setInterval(function () {
+        countdown();
+    }, 1000);
+}
